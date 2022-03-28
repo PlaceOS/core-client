2cc9dc16539196f37b58ea5e4c857630b981f90a
Stephen von Takach <steve@vontaka.ch>
2022-03-28 14:44:28 +1100

    fix: prevent retries on expected errors
    break up errors for improved retry handling and error propagation
    removed unused `ErrorCode` enum

--- src/error.cr
+++ src/error.cr
@@ -8,45 +8,42 @@ module PlaceOS::Core
   end
 
   class ClientError < Error
-    getter status_code
-    getter response_code
-    getter remote_backtrace
+    getter status_code : Int32
+    getter response_code : Int32
+    getter remote_backtrace : Array(String)?
 
-    enum ErrorCode
-      # The request was sent and error occured in core / the module
-      RequestFailed = 0
-      # Some other transient failure like database unavailable
-      UnexpectedFailure = 1
-
-      def to_s
-        super.underscore
-      end
-    end
-
-    def initialize(@status_code : Int32, message = "", @module_code : Int32 = 500)
+    def initialize(@status_code : Int32, message = "", @response_code : Int32 = 500)
       super(message)
     end
 
-    def initialize(path : String, @status_code : Int32, message : String, @module_code : Int32 = 500)
+    def initialize(path : String, @status_code : Int32, message : String, @response_code : Int32 = 500)
       super("request to #{path} failed with #{message}")
     end
 
-    def initialize(path : String, @status_code : Int32, @module_code : Int32 = 500)
+    def initialize(path : String, @status_code : Int32, @response_code : Int32 = 500)
       super("request to #{path} failed")
     end
 
     def initialize(
-      error_code : ErrorCode,
       @status_code : Int32,
       message : String = "",
       @remote_backtrace : Array(String)? = nil,
-      @module_code : Int32 = 500
+      @response_code : Int32 = 500
     )
       super(message)
     end
 
     def self.from_response(path : String, response : HTTP::Client::Response)
-      new(path, response.status_code, response.body)
+      new(path, response.status_code, response.body, response.headers["Response-Code"]?.try(&.to_i) || (response.success? ? 200 : 500))
     end
+  end
+
+  class DriverRaisedError < ClientError
+  end
+
+  class UnexpectedFailureError < ClientError
+  end
+
+  class APIResponseError < ClientError
   end
 end
